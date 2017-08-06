d632e316384acb4dfbe0e41416c90cd272b22d1a
Gabriele Ara <gabriele.ara@live.it>
2017-08-06 12:06:11 +0200

    RT-Druid Project creation
    Created the base project on which we will work from now on.

---
+++ sonar/Debug/eecfg.h
@@ -0,0 +1,97 @@
+#ifndef EECFG_H
+#define EECFG_H
+
+
+#define RTDRUID_CONFIGURATOR_NUMBER 1278
+
+
+
+/***************************************************************************
+ *
+ * Common defines ( CPU 0 )
+ *
+ **************************************************************************/
+
+    /* TASK definition */
+    #define EE_MAX_TASK 1
+    #define TaskIOToggle 0
+
+    /* MUTEX definition */
+    #define EE_MAX_RESOURCE 0U
+
+    /* ALARM definition */
+    #define EE_MAX_ALARM 1U
+    #define AlarmToggle 0U
+
+    /* SCHEDULING TABLE definition */
+    #define EE_MAX_SCHEDULETABLE 0U
+
+    /* COUNTER OBJECTS definition */
+    #define EE_MAX_COUNTER_OBJECTS (EE_MAX_ALARM + EE_MAX_SCHEDULETABLE)
+
+    /* COUNTER definition */
+    #define EE_MAX_COUNTER 1U
+    #define myCounter 0U
+
+    /* APPMODE definition */
+    #define EE_MAX_APPMODE 0U
+
+    /* CPUs */
+    #define EE_MAX_CPU 1
+    #define EE_CURRENTCPU 0
+
+    /* Number of isr 2 */
+    #define EE_MAX_ISR2   1
+    #define EE_MAX_ISR_ID 1
+
+#ifndef __DISABLE_EEOPT_DEFINES__
+
+
+/***************************************************************************
+ *
+ * User options
+ *
+ **************************************************************************/
+#define __USE_SYSTICK__
+#define __ADD_LIBS__
+
+
+/***************************************************************************
+ *
+ * Automatic options
+ *
+ **************************************************************************/
+#define __RTD_LINUX__
+#define __STM32__
+#define __STM32F4xx__
+#define __CORTEX_MX__
+#define __CORTEX_M4__
+#define __GNU__
+#define __FP__
+#define __MONO__
+#define __ALARMS__
+#define __FP_NO_RESOURCE__
+
+#endif
+
+
+
+/***************************************************************************
+ *
+ * ISR definition
+ *
+ **************************************************************************/
+#define EE_CORTEX_MX_SYSTICK_ISR systick_handler
+#define EE_CORTEX_MX_SYSTICK_ISR_PRI EE_ISR_PRI_1
+
+
+/***************************************************************************
+ *
+ * Vector size defines
+ *
+ **************************************************************************/
+    #define EE_ALARM_ROM_SIZE 1
+
+
+#endif
+
