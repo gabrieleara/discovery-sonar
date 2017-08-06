f338fa84d771d054a9fab2dbe0b90bfebf14e96b
Gabriele Ara <gabriele.ara@live.it>
2017-08-06 12:29:46 +0200

    Motor subsystem template
    Edit the added files to implement the motor subsystem.

---
+++ sonar/motor.c
@@ -0,0 +1,47 @@
+/*
+ * motor.c
+ *
+ * This file contains all the functions and tasks that interact with the servo
+ * motor used for this project.
+ *
+ * */
+
+// ---------------------------
+// Includes
+// ---------------------------
+
+#include "motor.h"
+
+/*
+ * Insert all includes here
+ * */
+
+
+
+// ---------------------------
+// Globals
+// ---------------------------
+
+/*
+ * Insert all globals here
+ * */
+
+
+
+// ---------------------------
+// Functions
+// ---------------------------
+
+/*
+ * Insert all functions here
+ * */
+
+
+
+// ---------------------------
+// Tasks
+// ---------------------------
+
+/*
+ * Insert all tasks here
+ * */
---
+++ sonar/motor.h
@@ -0,0 +1,47 @@
+/*
+ * motor.h
+ *
+ * This file contains all declaration of public functions and global variables
+ * defined in the motor.c file.
+ *
+ * */
+
+// ---------------------------
+// Includes
+// ---------------------------
+
+/*
+ * Insert all includes here
+ * */
+
+
+
+// ---------------------------
+// Data structures and data types
+// ---------------------------
+
+/*
+ * Insert all structures and data types here
+ * */
+
+
+
+// ---------------------------
+// Globals
+// ---------------------------
+
+/*
+ * Insert all globals here as extern variables
+ * */
+
+
+
+// ---------------------------
+// Functions
+// ---------------------------
+
+/*
+ * Insert all functions declarations here
+ * */
+
+
